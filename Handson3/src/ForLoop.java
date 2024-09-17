3d6305e9752114a2f0f7c874e3226af5390d33a3
Asafati Gea <2382006@unai.edu>
2024-09-17 13:47:26 +0700

    Contoh

---
+++ Handson3/src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // Contoh for loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iterasi ke-" + i);
+        }
+    }
+}
