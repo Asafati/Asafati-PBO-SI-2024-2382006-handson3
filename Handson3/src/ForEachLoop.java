690e81bfdd41d78e6c997984129fb26f2c42451e
Asafati Gea <2382006@unai.edu>
2024-09-17 13:45:50 +0700

    Contoh

---
+++ Handson3/src/ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+
+        // Enhanced for loop (for-each loop)
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
